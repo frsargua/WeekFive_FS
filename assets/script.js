a22d64276a08298d27dd7f49cfc007209bb8e39d
frsargua <frsargua@gmail.com>
2022-05-08 19:45:53 +0100

    updating renderDate function

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,6 @@
 // Get variable for input element
 var taskFormEl = $("#taskForm");
 var currentDayEl = $("#currentDay");
-var today = moment().format("h:mma , dddd, MMMM Do YYYY");
 
 // I need to create a function that can record the input of an input container
 
@@ -18,10 +17,9 @@ const handleSubmit = (event) => {
 // This function uses moment.js to obtain the current date, place it into the header and update it.
 
 const renderDate = () => {
-  timeIntervals = setInterval(function () {
-    currentDayEl.text(today);
-  }, 1000);
+  var today = moment().format("h:mm:ssa , dddd, MMMM Do YYYY");
+  currentDayEl.text(today);
 };
 
 taskFormEl.on("click", "#9", handleSubmit);
-renderDate();
+setInterval(renderDate, 1000);
