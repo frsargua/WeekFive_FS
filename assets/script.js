79f4d0c78e49f5cbb9fd9e0649e79bf9c745bd02
frsargua <frsargua@gmail.com>
2022-05-10 14:26:55 +0100

    Removed unnecesarry comment

--- assets/script.js
+++ assets/script.js
@@ -45,17 +45,6 @@ const workingHours = [
   },
 ];
 
-// I need to create a function that can record the input of an input container   <-- Texting function - not working
-// const handleSubmit = (event) => {
-//   event.preventDefault();
-//   let target = event.target.nextElementSibling;
-//   console.log(this);
-//   let newTarge = $(this).siblings("input").console.log(target);
-//   console.log(newTarge);
-//   let eventInputText = $('input[name="workPlanner"]').val();
-//   console.log(eventInputText);
-// };
-
 const renderTimeBlocks = (timeText, key) => {
   // Create div
   const divTimeBlockEl = $("<div>");
